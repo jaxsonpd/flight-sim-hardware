a88bcc9cdd9ceaafb4719c715c94d8fbc1ef91c4
Jack Duignan <jdu80@uclive.ac.nz>
2024-12-03 18:15:32 +1300

    displaying from serial

--- sim-firmware/sim-firmware.ino
+++ sim-firmware/sim-firmware.ino
@@ -36,6 +36,10 @@ void counting(void) {
     }
 }
 
+uint32_t frequency = 0;
+char receivedNumber[5];  // Buffer to store the received number (max 10 digits)
+int currentIndex = 0;     // Index for storing digits in the buffer
+    
 void loop() {
     // // put your main code here, to run repeatedly:
     // delay(1000);
@@ -47,18 +51,33 @@ void loop() {
     // event_transmit();
     // delay(1000);
     // display.update();
-    for (size_t i = 0; i < 10; i++)
-    {
-        multiDisplay.write(i*113);
-        
-        uint32_t start_time = millis();
-        while((millis() - start_time) < 1000) {
-            multiDisplay.update();
+
+    if (Serial.available() > 0) {
+        // Read one byte of data
+        char incomingByte = Serial.read();
+
+        // If we receive a semicolon, process the number
+        if (incomingByte == ';') {
+            receivedNumber[currentIndex] = '\0';  // Null-terminate the string
+
+            int number = atoi(receivedNumber);
+
+            Serial.print("Converted to int: ");
+            Serial.println(number);  
+            multiDisplay.write(number);
+
+            currentIndex = 0;
+        } 
+        else {
+            // Store the incoming byte in the buffer
+            if (currentIndex < 5) {
+                receivedNumber[currentIndex] = incomingByte;
+                currentIndex++;
+            }
         }
-        delay(100);
-
     }
-
+        
+    multiDisplay.update();
 
 }
 
